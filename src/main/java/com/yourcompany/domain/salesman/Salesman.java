37cf32f373981d95a87466465d4f5be36a045535
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2020-12-02 15:10:24 +0100

    save

--- src/main/java/com/yourcompany/domain/salesman/Salesman.java
+++ src/main/java/com/yourcompany/domain/salesman/Salesman.java
@@ -1,4 +1,12 @@
 package com.yourcompany.domain.salesman;
 
 public class Salesman {
+
+    private final int id;
+    private final int userId;
+
+    public Salesman(int id, int userId) {
+        this.id = id;
+        this.userId = userId;
+    }
 }
