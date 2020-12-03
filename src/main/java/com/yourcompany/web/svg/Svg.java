159d79b06abbc16bbaec249d91723597ee296f0c
jonasmik <jonassoldath@live.dk>
2020-12-03 13:08:37 +0100

    mere arbejde på svg

--- src/main/java/com/yourcompany/web/svg/Svg.java
+++ src/main/java/com/yourcompany/web/svg/Svg.java
@@ -66,8 +66,12 @@ public class Svg extends Tag {
         int lengthmm = length*10;
         Svg carportTopView = new Svg(lengthmm, widthmm, "0 0 9000 9000");
 
+        int pillarWidth = 97;
         int xspacing = 500;
         int yspacing = 500;
+        double upToFirstPillar = xspacing+1100-(pillarWidth/2);
+        double calculateMiddlePillar = ((xspacing+lengthmm-275-(pillarWidth/2))-(upToFirstPillar))/2+upToFirstPillar;
+
 
         // Tag
 
@@ -77,47 +81,54 @@ public class Svg extends Tag {
         // rem
         Tag topRim = new Rect(xspacing, yspacing+350, lengthmm, 45);
         Tag bottomRim = new Rect(xspacing, yspacing+widthmm-350-45, lengthmm, 45);
-        carportTopView.add(topRim.withStyle("fill: white; stroke: black; stroke-width: 15;"));
-        carportTopView.add(bottomRim.withStyle("fill: white; stroke: black; stroke-width: 15;"));
+        carportTopView.add(topRim.withStyle("fill: white; stroke: black; stroke-width: 10;"));
+        carportTopView.add(bottomRim.withStyle("fill: white; stroke: black; stroke-width: 10;"));
 
         // Den minimale længde før at vi er nød til at have 3 stolper per side i carporten
         // Udhæng før første stolpe + max afstanden mellem 2 stolper + udhæng bag til
         double minLengthThirdPost = 1100+(550*5.5)+275;
 
-        if (lengthmm >= minLengthThirdPost){
-            Tag topMiddlePost = new Rect((xspacing+lengthmm-275-48.5)-(xspacing+1100-48.5), yspacing+350, 97, 97);
-            Tag bottomMiddlePost = new Rect(xspacing+1100-48.5, yspacing+width-350-97, 97, 97);
-            carportTopView.add(topMiddlePost.withStyle("fill: white; stroke: black; stroke-width: 15;"));
-            carportTopView.add(bottomMiddlePost.withStyle("fill: white; stroke: black; stroke-width: 15;"));
-        }
+
 
         // Spær
-        int x = 0;
-        while (x < lengthmm){
+        double calculateAmountOfRafters = lengthmm/(550+45);
+        double calculateExtraRaftersSpace = ((lengthmm%(550+45)))/calculateAmountOfRafters;
+
+        System.out.println(calculateExtraRaftersSpace);
+        System.out.println(calculateAmountOfRafters);
+        double x = 0;
+        while (x <= lengthmm){
             Tag rafter = new Rect( xspacing-22.5+x, yspacing, 45, widthmm);
-            carportTopView.add(rafter.withStyle("fill: white; stroke: black; stroke-width: 15;"));
-            x=x+550;
+            carportTopView.add(rafter.withStyle("fill: white; stroke: black; stroke-width: 10;"));
+            x=x+550+calculateExtraRaftersSpace;
         }
 
-        // Stolper 1100 = udhæng foran, 48.5 = halvdelen af stolpe, 275 = halvdelen af remmen
-        Tag topLeftPost = new Rect(xspacing+1100-48.5, yspacing+350, 97, 97);
-        Tag topRightPost = new Rect(xspacing+lengthmm-275-48.5, yspacing+350, 97, 97);
-        carportTopView.add(topLeftPost.withStyle("fill: white; stroke: black; stroke-width: 15;"));
-        carportTopView.add(topRightPost.withStyle("fill: white; stroke: black; stroke-width: 15;"));
+        // Stolper 1100 = udhæng foran, 48.5 = halvdelen af stolpe, 275 = centeret af mellemrummet mellem spær
+        Tag topLeftPost = new Rect(upToFirstPillar, yspacing+350, pillarWidth, pillarWidth);
+        Tag topRightPost = new Rect(xspacing+lengthmm-275-(pillarWidth/2), yspacing+350, pillarWidth, pillarWidth);
+        carportTopView.add(topLeftPost.withStyle("fill: white; stroke: black; stroke-width: 10;"));
+        carportTopView.add(topRightPost.withStyle("fill: white; stroke: black; stroke-width: 10;"));
 
-        Tag bottomLeftPost = new Rect(xspacing+1100-48.5, yspacing+widthmm-350-97, 97, 97);
-        Tag bottomRightPost = new Rect(xspacing+lengthmm-275-48.5, yspacing+widthmm-350-97, 97, 97);
-        carportTopView.add(bottomLeftPost.withStyle("fill: white; stroke: black; stroke-width: 15;"));
-        carportTopView.add(bottomRightPost.withStyle("fill: white; stroke: black; stroke-width: 15;"));
+        Tag bottomLeftPost = new Rect(upToFirstPillar, yspacing+widthmm-350-pillarWidth, pillarWidth, pillarWidth);
+        Tag bottomRightPost = new Rect(xspacing+lengthmm-275-(pillarWidth/2), yspacing+widthmm-350-pillarWidth, pillarWidth, pillarWidth);
+        carportTopView.add(bottomLeftPost.withStyle("fill: white; stroke: black; stroke-width: 10;"));
+        carportTopView.add(bottomRightPost.withStyle("fill: white; stroke: black; stroke-width: 10;"));
 
-        // Stern MÅSKE IKKE NØDVENDIG?
+        if (lengthmm >= minLengthThirdPost){
+            Tag topMiddlePost = new Rect(calculateMiddlePillar, yspacing+350, pillarWidth, pillarWidth);
+            Tag bottomMiddlePost = new Rect(calculateMiddlePillar, yspacing+widthmm-350-pillarWidth, pillarWidth, pillarWidth);
+            carportTopView.add(topMiddlePost.withStyle("fill: white; stroke: black; stroke-width: 10;"));
+            carportTopView.add(bottomMiddlePost.withStyle("fill: white; stroke: black; stroke-width: 10;"));
+        }
+
+        /* Stern MÅSKE IKKE NØDVENDIG?
         Tag frontUnderStern = new Rect(xspacing,yspacing,25,widthmm);
         Tag backUnderStern = new Rect(xspacing,yspacing+lengthmm-25,25,widthmm);
 
         // Her skal vi ændre længden så den har fået trukket bredden af sternbræderne som sidder på front og bagende
         Tag topUnderStern = new Rect(xspacing+25,yspacing,lengthmm-50,25);
         Tag bottomUnderStern = new Rect(xspacing+25,yspacing+widthmm-25,lengthmm-50,25);
-
+        */
 
 
         return carportTopView;
@@ -125,7 +136,7 @@ public class Svg extends Tag {
 
     public static void main(String[] args) {
         try (FileWriter writer = new FileWriter("output.svg")) {
-            writer.write(carportTopView(500, 400).toString());
+            writer.write(carportTopView(800, 550).toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
